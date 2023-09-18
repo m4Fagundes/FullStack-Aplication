f7aff1b5578ab5444b57cc14cc23e3009698deb9
mFagundes <matheusfagundesaraujo@gmail.com>
2023-09-18 17:08:26 -0300

    primeiros passos fullStack aplication

---
+++ src/main/java/com/crudTeste/fullstackbackend/controller/UsarControler.java
@@ -0,0 +1,21 @@
+package com.crudTeste.fullstackbackend.controller;
+
+
+import com.crudTeste.fullstackbackend.model.User;
+import com.crudTeste.fullstackbackend.repository.UserRapository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsarControler {
+
+    @Autowired
+    private UserRapository userRapository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRapository.save(newUser);
+    }
+}
